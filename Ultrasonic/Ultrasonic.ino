1c89ad7da6723a1d63eefded93ac6837437a2b37
SmartBin4GreenU <35843627+SmartBin4GreenU@users.noreply.github.com>
2018-04-21 02:11:41 +0700

    update code

--- Ultrasonic/Ultrasonic.ino
+++ Ultrasonic/Ultrasonic.ino
@@ -1,3 +1,8 @@
+/*
+  Programer : Sarawut Waiyakorn  5635512044
+  Project: SmartBin4GreenUniversity
+*/
+
 #include <NewPingESP8266.h>
 #include <Firebase.h>
 #include <FirebaseArduino.h>
@@ -5,15 +10,22 @@
 #include <FirebaseError.h>
 #include <FirebaseHttpClient.h>
 #include <FirebaseObject.h>
+
 /**********************************************************************/
+
 #include <ESP8266WiFi.h>
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>    
 
 /**********************************************************************/
+
 #define FIREBASE_HOST "sb-fgu.firebaseio.com"
 #define FIREBASE_AUTH "1CMvHocG4TPd2ivn1YTWKDcI7B9f3AMLNGI0vKHz"
+
+///*Config  connect to Firebase*/
+//#define FIREBASE_HOST "smartbin4greenuniversity.firebaseio.com"
+//#define FIREBASE_AUTH "qeparmANzkz2h60KJ986rT1Y3D3VsvfpCCylbNdR"
 
 #define TRIGGER_PIN  D3
 #define ECHO_PIN     D4
@@ -22,7 +34,7 @@
 #define ECHO_PIN2     D6
 #define MAX_DISTANCE2 300
 
- /*********************************************************************/
+/*********************************************************************/
 NewPingESP8266 sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 NewPingESP8266 sonar2(TRIGGER_PIN2, ECHO_PIN2, MAX_DISTANCE2);
  
@@ -32,7 +44,7 @@ void setup()
       Serial.begin(115200);  
       pinMode(LED_BUILTIN,OUTPUT);
       WiFiManager wifiManager;
-      wifiManager.autoConnect("SB_FGU AP1");
+      wifiManager.autoConnect("SB_FGU Ultrasonic AP1");
       while (WiFi.status() != WL_CONNECTED) 
       {
            delay(250);
@@ -47,7 +59,7 @@ void setup()
 int MAX = 10;
 int Dis1[10] = {};
 int Dis2[10] = {};
-int Count =0;
+int Count = 0;
 float Sum[10] = {};
 int Average = 0;
 int Round = 0;
@@ -67,7 +79,7 @@ void loop()
    Serial.print(" cm");  
    Serial.println("  ");
    
-     if(Count >= MAX ){
+     if(Count >= MAX ) {
           for(int i = 0; i < MAX; i++){
               Sum[i] = Dis1[i] + Dis2[i];
               Average += Sum[i]/2;
@@ -79,7 +91,7 @@ void loop()
           Count = 0;
           Round = 0;
       }
-    else{
+    else {
         if(Round < MAX){
           Dis1[Round] = Distance1 ;
           Dis2[Round] = Distance2 ;
@@ -97,8 +109,7 @@ void loop()
         }
         Round++;
       }
-//     Serial.print("                                         Round: ");
-//     Serial.println(Count);
      Count++;
      delay(1000);
 } 
+
